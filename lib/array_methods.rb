3b384e1ea452dc389115a4478000f9679a8f757d
Sean Fyffe <stfyffe@gmail.com>
2019-09-30 07:18:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,19 +9,20 @@ end
 
 def find_max_value(array)
   x = array[0]
-  array.length.times {|index|
+  array.length.times do |index|
     if array[index] > x
       x = array[index]
     end
-    }
+  end
   x
 end
 
 def find_min_value(array)
   x = array[0]
-  array.length.times{|index|
+  array.length.times do|index|
     if array[index] < x
       x = array[index]
     end
-  }
+  end
+  x
 end
