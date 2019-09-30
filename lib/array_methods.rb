0afd17eec61fe55e8254a4757da4c446a59897a0
Sean Fyffe <stfyffe@gmail.com>
2019-09-30 07:11:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,11 +9,11 @@ end
 
 def find_max_value(array)
   x = 0
-  array.length.times do |index|
+  array.length.times {|index|
     if array[index > x
       x = array[index]
     end
-  end
+  }
   x
 end
 
